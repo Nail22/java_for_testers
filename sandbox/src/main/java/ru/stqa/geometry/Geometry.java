20a2d4963a227b76b8c7ef1ef0ceaf833ef67491
Nail4ilL <nail.valishin.2000@mail.ru>
2025-04-19 18:10:11 +0500

    Начало модуля по функциональному программированию

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -4,16 +4,22 @@ import ru.stqa.geometry.figures.Rectangle;
 import ru.stqa.geometry.figures.Square;
 import ru.stqa.geometry.figures.Triangle;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Geometry {
     public static void main(String[] args) {
-        Square.printSquareArea(new Square(7.0));
-        Square.printSquareArea(new Square(5.0));
-        Square.printSquareArea(new Square(3.0));
+        var squares = List.of(new Square(7.0),new Square(5.0),new Square(3.0));
+//        for (Square square : squares) {
+//            Square.printSquareArea(square);
+//        }
+//        Consumer<Square> print = Square::printSquareArea;
+        squares.forEach(Square::printSquareArea);
         Triangle.printTriangleArea( new Triangle(9, 6, 8));
         Triangle.printTrianglePerimeter(new Triangle(9, 6, 8));
 
-        Rectangle.printRectangleArea(3.0, 5.0);
-        Rectangle.printRectangleArea(6.0, 7.0);
+//        Rectangle.printRectangleArea(3.0, 5.0);
+//        Rectangle.printRectangleArea(6.0, 7.0);
     }
 
 }
