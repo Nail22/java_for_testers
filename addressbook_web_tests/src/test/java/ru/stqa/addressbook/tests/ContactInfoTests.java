ab44912d1169a69ee242ad4706136fd8a4622f73
Nail4ilL <nail.valishin.2000@mail.ru>
2025-04-23 22:30:30 +0500

    Реализовал тест для проверки информации о контактах на главной странице

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactInfoTests.java
@@ -9,49 +9,30 @@ import java.util.stream.Stream;
 
 public class ContactInfoTests extends TestBase {
 
+
     @Test
-    void testPhones(){
+    void TestInfoContact() {
         var contacts = app.hbm().getContactList();
-        var expected =  contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
-                Stream.of(contact.home(),contact.mobile(),contact.work(),contact.secondary())
-                        .filter(s->s !=null&& !"".equals(s))
+        var expectedPhones = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+                Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary())
+                        .filter(s -> s != null && !"".equals(s))
+                        .collect(Collectors.joining("\n"))
+        ));
+        var expectedMail = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+                Stream.of(contact.email(), contact.email2(), contact.email3())
+                        .filter(s -> s != null && !"".equals(s))
+                        .collect(Collectors.joining("\n"))
+        ));
+        var expectedAddress = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+                Stream.of(contact.address())
+                        .filter(s -> s != null && !"".equals(s))
                         .collect(Collectors.joining("\n"))
         ));
         var phones = app.contact().getPhones();
-        Assertions.assertEquals(expected, phones);
-    }
-    @Test
-    void testMail(){
-        var contacts = app.hbm().getContactList();
-        var expected =  contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
-                Stream.of(contact.email(),contact.email2(),contact.email3())
-                        .filter(s->s !=null&& !"".equals(s))
-                        .collect(Collectors.joining("\n"))
-        ));
         var mail = app.contact().getMail();
-        Assertions.assertEquals(expected,mail);
-    }
-
-    @Test
-    void TestAddress(){
-        var contacts = app.hbm().getContactList();
-        var expected =  contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
-                Stream.of(contact.address())
-                        .filter(s->s !=null&& !"".equals(s))
-                        .collect(Collectors.joining("\n"))
-        ));
         var address = app.contact().getAddress();
-        Assertions.assertEquals(expected,address );
-    }
-    @Test
-    void TestContentContact(){
-        var contacts = app.hbm().getContactList();
-        var expected =  contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
-                Stream.of(contact.lastName(),contact.firstName(),contact.address(),contact.email(),contact.email2(),contact.email3(),contact.home(),contact.mobile(),contact.work(),contact.secondary())
-                        .filter(s->s !=null&& !"".equals(s))
-                        .collect(Collectors.joining("\n"))
-        ));
-        var contentContact = app.contact().getContentContact();
-        Assertions.assertEquals(expected,contentContact);
+        Assertions.assertEquals(expectedPhones, phones);
+        Assertions.assertEquals(expectedMail, mail);
+        Assertions.assertEquals(expectedAddress, address);
     }
 }
